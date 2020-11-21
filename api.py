0310e59666429a9164dc0e73ce1305ef37ec6fdd
muckas <motobike8@outlook.com>
2020-11-21 19:46:53 +0500

    Add wallet frame displaying total balance

--- api.py
+++ api.py
@@ -6,3 +6,18 @@ with open('secret') as f:
   api_secret = f.read().rstrip('\n')
 
 polo = Poloniex(key=api_key, secret=api_secret)
+
+def getTotalBTC():
+  data = polo.returnCompleteBalances()
+  for currency in data.copy():
+    if data[currency]['btcValue'] == '0.00000000':
+      data.pop(currency)
+  totalBtc = 0.0
+  for currency in data:
+    totalBtc += float(data[currency]['btcValue'])
+  return totalBtc
+
+# TESTING
+if __name__ == '__main__':
+  output = getTotalBalance()
+  print(output)
--- main.py
+++ main.py
@@ -7,21 +7,35 @@ root.title('Poloniex Client')
 img = PhotoImage(file = 'polo.png')
 root.iconphoto(True, img)
 tickerFrame = LabelFrame(root, text='Ticker', padx=5, pady=5)
-tickerFrame.pack()
+tickerFrame.grid(row=0, column=0, padx=5, pady=5)
+walletFrame = LabelFrame(root, text='Wallet', padx=5, pady=5)
+walletFrame.grid(row=0, column=1, padx=5, pady=5)
 
-pairEnt = Entry(tickerFrame, width=10)
-pairEnt.grid(row=0, column=0)
-pairEnt.insert(0, 'USDT_BTC')
+# TICKER FRAME
+tickerPairEnt = Entry(tickerFrame, width=10)
+tickerPairEnt.grid(row=0, column=0)
+tickerPairEnt.insert(0, 'USDT_BTC')
 
-priceLbl = Label(tickerFrame, text=' ')
-priceLbl.grid(row=0, column=1)
+tickerPriceLbl = Label(tickerFrame, text='0.00000000')
+tickerPriceLbl.grid(row=0, column=1)
 
-def click():
+def tickerRefreshBtnClick():
   data = polo.returnTicker()
-  pair = pairEnt.get()
-  priceLbl.config(text=data[pair]['last'])
+  pair = tickerPairEnt.get()
+  tickerPriceLbl.config(text=data[pair]['last'][:10])
 
-refreshBtn = Button(tickerFrame, text='Refresh', command=click)
-refreshBtn.grid(row=1)
+tickerRefreshBtn = Button(tickerFrame, text='Refresh', command=tickerRefreshBtnClick)
+tickerRefreshBtn.grid(row=1)
+
+# WALLET FRAME
+walletBalanceLbl = Label(walletFrame, text='Balance')
+walletBalanceLbl.grid(row=0, column=0)
+
+def walletRefreshBtnClick():
+  balance = api.getTotalBTC()
+  walletBalanceLbl.config(text=balance)
+
+walletRefreshBtn = Button(walletFrame, text='Refresh', command=walletRefreshBtnClick)
+walletRefreshBtn.grid(row=1)
 
 root.mainloop()
